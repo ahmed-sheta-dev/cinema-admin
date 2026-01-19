0679abbbdc9853424737b56deffd2119443a5f55
Ahmed Sheta <ahmed.shetaa.2003@gmail.com>
2026-01-19 16:28:02 +0200

    feat: add alembic baseline revision and stamp

---
+++ migration/versions/181a19ee009f_baseline.py
@@ -0,0 +1,28 @@
+"""baseline
+
+Revision ID: 181a19ee009f
+Revises: 
+Create Date: 2026-01-19 16:25:14.176755
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '181a19ee009f'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
